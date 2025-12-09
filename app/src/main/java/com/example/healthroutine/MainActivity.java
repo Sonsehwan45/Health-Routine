a792b2aed0fc3d67275dee8ae67ebb65fb29a1a8
Sonsehwan45 <sahwan458@gmail.com>
2025-12-10 06:00:57 +0900

    [feat] main page 화면 구현

--- app/src/main/java/com/example/healthroutine/MainActivity.java
+++ app/src/main/java/com/example/healthroutine/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.healthroutine;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,9 +12,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+        Button btCreateRoutine = findViewById(R.id.bt_create_routine);
 
+        btCreateRoutine.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, CreateRoutine.class);
+            startActivity(intent);
+            Log.d("MainPage", "루틴 생성 버튼 클릭");
+        });
     }
 }
